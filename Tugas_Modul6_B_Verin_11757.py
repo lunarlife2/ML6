573c5f97b430629c3df2e5e24656e3ca2354661d
Averina Harseno Subianto <156516558+lunarlife2@users.noreply.github.com>
2024-12-04 15:47:52 +0700

    Update Tugas_Modul6_B_Verin_11757.py

--- Tugas_Modul6_B_Verin_11757.py
+++ Tugas_Modul6_B_Verin_11757.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = load_model(r'D:\atma\Semester 5\ML\Tugas6_B_11757\model_mobilenet.h5')
+model = load_model(r'model_mobilenet.h5')
 class_names = ['Matang', 'Mentah']
 
 def classify_image(image_path):
